878bcce87116f43b1294b791f414ef8edd85b740
BM-Daniel <danbabereyir@gmail.com>
2022-10-14 14:37:51 -0700

    Done with task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,14 +3,11 @@
 #include <stdio.h>
 
 /**
- * main - the entry point of the program
+ * main - entry point
  *
- * Description: This program will assign a random number
- * to the variable n each time it is executed
- * Complete the source code in order to print the last
- * digit of the number stored in the variable n
+ * Description:
  *
- * Return: Always exit with code 0
+ * Return: Always 0 (SUCCESS)
  */
 
 int main(void)
@@ -19,7 +16,7 @@ int main(void)
 	int last_digit;
 
 	srand(time(0));
-	n = rand () - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
 	last_digit = n % 10;
 
