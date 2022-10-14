a18b5f7039b3836a12eae1defe6c98a7842f2dc3
BM-Daniel <danbabereyir@gmail.com>
2022-10-13 18:03:27 -0700

    Task 8 file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,13 @@
 /**
  *  main - entry point
  *
- *  description - print out a statement to the standard error
+ *  Description - print out a statement to the standard error
  *
- *  return - always exit with code 1
+ *  Return - always exit with code 1
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
