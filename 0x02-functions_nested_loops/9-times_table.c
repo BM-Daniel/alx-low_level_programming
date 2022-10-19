511cc7ed6b2a5bc59c8d69da882fdcba3a0402d4
BM-Daniel <danbabereyir@gmail.com>
2022-10-18 19:09:46 -0700

    Fix error in task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,6 +18,11 @@ void times_table(void)
 			result = i * j;
 			left_digit = result / 10;
 			right_digit = result % 10;
+			
+			if (j == 0)
+			{
+				_putchar('0');
+			}
 
 			if (result <= 9)
 			{
