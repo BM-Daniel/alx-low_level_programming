729b0910a1a1fb35a7ea0445680536afa340c8c1
BM-Daniel <danbabereyir@gmail.com>
2022-10-20 04:39:09 -0700

    Add file for task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-/**
-* while - loop through a condition
-*	while (i < 10)
-*	{
-*		putchar(i);
-*	}
-*/
+	/**
+	while (i < 10)
+	{
+		putchar(i);
+	}
+	*/
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
