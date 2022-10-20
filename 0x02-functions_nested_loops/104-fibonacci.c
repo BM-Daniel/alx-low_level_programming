3b0b16ff34ec77dca30cc8cc5371bdf1fed86241
BM-Daniel <danbabereyir@gmail.com>
2022-10-19 18:37:48 -0700

    Add files for tasks 14-16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Write a program that prints the first 98 Fibonacci
+ * numbers, starting with 1 and 2, followed by a new line
+ *
+ * Return: Exit 0 as success
+ */
+
+int main(void)
+{
+	unsigned int i, c;
+	unsigned int a = 1;
+	unsigned int b = 2;
+
+	printf("%u, ", a);
+	printf("%u, ", b);
+
+	for(i = 3; i <= 98; i++)
+	{
+		c = a + b;
+		if (i == 98)
+		{
+			printf("%u", c);
+			continue;
+		}
+
+		printf("%u, ", c);
+
+		a = b;
+		b = c;
+	}
+	printf("\n");
+
+	return (0);
+}
