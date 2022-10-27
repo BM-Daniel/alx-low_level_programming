61bd6a2e5bc9dae3a2a5f89043feac5df8d1c023
BM-Daniel <danbabereyir@gmail.com>
2022-10-27 03:52:05 -0700

    Fix error in task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strncat - Write a function that concatenates two strings
@@ -14,10 +15,16 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int j = 0;
+	int dest_size = 98;
 
 	while (dest[i] != '\0')
 	{
 		i++;
+	}
+
+	if (n > (dest_size - i))
+	{
+		n = dest_size - i;
 	}
 
 	while (j < n)
