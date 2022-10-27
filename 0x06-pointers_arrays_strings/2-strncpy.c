40f7827a400596cf1b7c3c049a976d0565e2f5e6
BM-Daniel <danbabereyir@gmail.com>
2022-10-27 04:20:30 -0700

    Fix error in task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,10 +14,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n)
+	while (i < n && src[i] != '\0')
 	{
 		*(dest + i) = *(src + i);
 		i++;
+	}
+
+	if (src[i] == '\0')
+	{
+		*(dest + i) = '\0';
 	}
 
 	return (dest);
