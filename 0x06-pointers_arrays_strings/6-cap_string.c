a29c9a88fa8f9639f28c0d192036f06404936850
BM-Daniel <danbabereyir@gmail.com>
2022-10-28 15:53:39 -0700

    Add files for task 6 and 7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,6 +18,10 @@ char *cap_string(char *str)
 	{
 		for (j = 0; separators[j] != '\0'; j++)
 		{
+			if (str[0] >= 97 && str[0] <= 122)
+			{
+				str[0] = str[0] - 32;
+			}
 			if (str[i] == separators[j])
 			{
 				if (str[i + 1] >= 97 && str[i + 1] <= 122)
