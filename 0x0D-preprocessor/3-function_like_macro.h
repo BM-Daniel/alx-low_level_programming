d540701339af313c045eb48096e5c840bb95f224
BM-Daniel <danbabereyir@gmail.com>
2022-11-15 12:51:45 -0800

    Add files for all tasks

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * Write a function-like macro ABS(x) that computes the absolute value of a
+ * number x
+ *
+ * @x: Integer value
+ */
+
+#define ABS(x) (x < 0 ? -x : x)
+
+#endif
