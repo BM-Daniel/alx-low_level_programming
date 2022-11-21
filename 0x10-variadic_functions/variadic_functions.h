0ee7018188c8543c9231178d37f2b6e843552a94
BM-Daniel <danbabereyir@gmail.com>
2022-11-21 03:19:58 -0800

    Task 3 files

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,28 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
 int _putchar(char letter);
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+
+/**
+ * struct printer - A strucutre to inform the print functions what to print
+ *
+ * @type: Represents the data type of info to be printed
+ * @print: A function pointer needed to print the info
+ */
+typedef struct printer
+{
+	char *type;
+	void (*print)(va_list arguments);
+} struct_blueprint;
+
+
 void print_all(const char * const format, ...);
 
 #endif
