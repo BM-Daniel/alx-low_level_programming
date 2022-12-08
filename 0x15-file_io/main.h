2188f51a54c9d229f40f489360bbf687ed93c36e
BM-Daniel <danbabereyir@gmail.com>
2022-12-08 15:08:34 -0800

    Add tasks 1 and 2 files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,9 +5,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 int _putchar(char letter);
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
