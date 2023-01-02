85724cbef57b0e62ca0fee8591e57b29d4c0d04a
BM-Daniel <danbabereyir@gmail.com>
2023-01-02 15:03:48 -0800

    Fix error in task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,11 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_list - Write a function that prints all the elements of a list_t list
- * @h: Pointer to linnked list
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
  *
- * Return: Number of nodes. If str is NULL, print [0] (nil)
+ * Return: The number of nodes in h.
  */
 
 size_t print_list(const list_t *h)
