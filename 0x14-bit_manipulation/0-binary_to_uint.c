3891c4eaf5b32f675a6d89c460e81045a1a73acb
BM-Daniel <danbabereyir@gmail.com>
2023-01-02 04:11:41 -0800

    Make correction in task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,20 +7,26 @@
  * Return: If b is NULL or contains chars not 0 or 1 - 0.
  *         Otherwise - the converted number.
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int len = 0;
+	unsigned int base_var = 1, res = 0, len = 0;
 
-	if (b[len] == '\0')
+	if (b == NULL)
 		return (0);
 
-	while ((b[len] == '0') || (b[len] == '1'))
-	{
-		num <<= 1;
-		num += b[len] - '0';
+	while (b[len])
 		len++;
-	}
 
-	return (num);
+	while (len)
+	{
+		if (b[len - 1] != '0' && b[len - 1] != '1')
+			return (0);
+
+		if (b[len - 1] == '1')
+			res += base_var;
+		base_var *= 2;
+		len--;
+	}
+	return (res);
 }
