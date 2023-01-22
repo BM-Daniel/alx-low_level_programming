7a9274be108780747491e70c12c047b783750de3
BM-Daniel <danbabereyir@gmail.com>
2023-01-22 15:02:13 -0800

    Make corrections to files

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -9,7 +9,7 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *node;
 
-	while (node != NULL)
+	while (node)
 	{
 		node = head->next;
 		free(head);
--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -17,6 +17,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	while (index > 0)
 	{
+		if (head == NULL)
+			return (NULL);
+
 		node = node->next;
 		index--;
 	}
