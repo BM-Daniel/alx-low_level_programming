eff5dd2ec09fb5d3b898b65ec73ef8987969ae60
BM-Daniel <danbabereyir@gmail.com>
2023-05-08 18:54:26 +0000

    betty

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,10 +3,10 @@
 /**
  * add_nodeint_end - Write a function that adds a new node at the
  * end of a listint_t list
- * 
+ *
  * @head: Pointer to head of the list
  * @n: Integer for new node
- * 
+ *
  * Return: the address of the new element, or NULL if it failed
 */
 
