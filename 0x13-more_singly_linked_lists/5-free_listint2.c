63da572e4f885a05e246e791839422de9fff5540
BM-Daniel <danbabereyir@gmail.com>
2023-05-08 18:43:27 +0000

    Indentation

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,19 +9,19 @@
 
 void free_listint2(listint_t **head)
 {
-    listint_t *temp;
+	listint_t *temp;
 
-    if (head == NULL)
-        return;
-    else
-    {
-        while (*head)
-        {
-            temp = (*head)->next;
-            free(*head);
-            *head = temp;
-        }
-    }
+	if (head == NULL)
+		return;
+	else
+	{
+		while (*head)
+		{
+			temp = (*head)->next;
+			free(*head);
+			*head = temp;
+		}
+	}
 
-    head = NULL;
+	head = NULL;
 }
