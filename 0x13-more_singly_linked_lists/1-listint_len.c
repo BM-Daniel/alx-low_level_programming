1dcf0f4758722d106f2d2995040a0e3fd8eb4bcb
BM-Daniel <danbabereyir@gmail.com>
2023-05-08 18:48:04 +0000

    Fix betty issues

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,13 +10,13 @@
 
 size_t listint_len(const listint_t *h)
 {
-    size_t node = 0;
+	size_t node = 0;
 
-    while (h)
-    {
-        node++;
-        h = h->next;
-    }
+	while (h)
+	{
+		node++;
+		h = h->next;
+	}
 
-    return (node);
+	return (node);
 }
